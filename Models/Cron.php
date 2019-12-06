160d7533ac559332de4c8ece69ea2682edeeedcf
Vincentz Jean <55762464+Slide-code@users.noreply.github.com>
2019-12-06 13:59:11 +0100

    Ajout code test (Cron_modeils).

--- Models/Cron.php
+++ Models/Cron.php
@@ -1,9 +1,10 @@
 <?php 
-
-
 require('Connexion.php');
 
-function __construct($host=null, $port=null, $user=null, $password=null){}
-
-
+function date_expiration(){
+    $current_date = date("m.d.y");
+    $sql = "DELETE FROM 'z-files' WHERE 'date' >= NOW()";
+    PDO::Exec($sql) or die('Erreur SQL !'.$sql.'<br>'.(0));
+}
+?>
 
